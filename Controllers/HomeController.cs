d304c68517b7b303ca1a8a2c663457e6482f8543
jxp359 <jxp359>
2020-02-01 16:07:51 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPrueba.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "ES ARTESANO";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "ES USUARIO";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "ES ARTESANO";
+
+            return View();
+        }
+    }
+}
